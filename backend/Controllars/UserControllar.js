ab16fe5dac600b4c9c286e27f59036b701532f31
itzadmin001 <158562740+itzadmin001@users.noreply.github.com>
2025-08-31 10:34:49 +0530

    ek or

--- backend/Controllars/UserControllar.js
+++ backend/Controllars/UserControllar.js
@@ -35,7 +35,7 @@ async function CreateUser(req, res) {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            sameSite: "none",
             maxAge: 12 * 60 * 60 * 1000
         })
         return res.status(201).json({
@@ -75,7 +75,7 @@ async function LoginUser(req, res) {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            sameSite: "none",
             maxAge: 12 * 60 * 60 * 1000
         })
         return res.status(200).json({
